4e18e55048f56ea361f87a0e32acf64a1d5634c1
AngeloRoudani <ange.rouda@gmail.com>
2023-05-22 15:33:29 +0200

    first commit

---
+++ database/migrations/2023_05_22_122449_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)->unique();
+            $table->string('language_dev', 20);
+            $table->string('framework', 50);
+            $table->date('start_date');
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.admin')
+@section('content')
+
+    <h1>Create your Project Portfolio</h1>
+@endsection
